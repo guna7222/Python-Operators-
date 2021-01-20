da4638bd1009295337f36eb3b929955e645bcb6d
guna7222 <47294637+guna7222@users.noreply.github.com>
2021-01-20 21:56:07 +0530

    Initial Commit

---
+++ Arthmetic operators.py
@@ -0,0 +1,38 @@
+# Arthmetic operators
+
+# Addition(+)
+
+a = 10
+b = 90
+print(a+b)
+
+# Subtraction(-)
+
+a = 90
+b = 39
+print(a-b)
+
+# Multiplication (*)
+
+a = 83
+b = 83
+print(a*b)
+
+# Division (/)
+
+a = 15
+b = 3
+print(a/b)
+
+#   Exponentiation (**)
+
+a = 12
+b = 2
+print(a**b)
+
+# Floor Division (//)
+
+a = 22
+b = 7
+print(a//b)
+
