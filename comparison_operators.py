6e466fab3936d1be33bcb3cbe957edbc5f64d0c9
guna7222 <47294637+guna7222@users.noreply.github.com>
2021-01-26 21:08:48 +0530

    Second commit

---
+++ comparison_operators.py
@@ -0,0 +1,38 @@
+#Comparison operators
+# (==) Equal
+
+a = 10
+b = 5
+print( a==b)
+
+# (!=) Not Equal
+
+a = 10
+b = 5
+print( a!=b)
+
+# (>) Greater than
+
+a = 10
+b = 5
+print( a>b)
+
+# (<) Less than
+
+a = 10
+b = 5
+print( a<b)
+
+# (>=) Greater than or Equal too
+
+a = 10
+b = 5
+print( a>=b)
+
+# (>=) Less than or Equal too
+
+a = 10
+b = 5
+print( a<=b)
+
+# final output would in the form of bool 
