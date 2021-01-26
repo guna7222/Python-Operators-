48dcbc8a0aedd6e01e338dbf59c7ca5259be3daf
guna7222 <47294637+guna7222@users.noreply.github.com>
2021-01-26 21:30:23 +0530

    Third commit

---
+++ logical operators.py
@@ -0,0 +1,24 @@
+#Logical operators
+"""
+1)and
+2)or
+3)not
+"""
+
+#and :- returns True if both statements are True
+
+a = 10
+b = 45
+print(b>a and a<b)
+
+#or :- returns True if any one statement is True
+
+a = 10
+b = 45
+print(a>b or a<b)
+
+# not:- reverse the result 
+
+a = 10
+b = 45
+print(not(a<b and b>a))
