b7b2e7eab623026b0ab1f602a2cca48da7a71709
guna7222 <47294637+guna7222@users.noreply.github.com>
2021-01-28 21:37:02 +0530

    Commit

---
+++ membership operator.py
@@ -0,0 +1,16 @@
+""" Membership operators
+1) in
+2) not in 
+"""
+txt = 'hello world'
+print('w' in txt)
+
+# Returns True, if a sequence with specified value is present in the object 
+
+# not in
+
+# Returns True if a sequence with the specified value is not present in the object
+
+txt = 'hello world'
+print('w' not in txt)
+
